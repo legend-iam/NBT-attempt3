7cba95e70581aa602b3b4a3406c107331471e47c
Prakash Bansal <prakashb13@gmail.com>
2018-01-02 12:37:22 +0530

    database changes shown in the multi-line comments

--- app/src/main/java/pbapps/nbt_attempt3/MyDBAdapter.java
+++ app/src/main/java/pbapps/nbt_attempt3/MyDBAdapter.java
@@ -29,13 +29,48 @@ public class MyDBAdapter {
     public static final String COL_STOP_3_X = "X_3";
     public static final String COL_STOP_3_Y = "Y_3";
 
+    /*
+    *
+    * private static final String DB_SAMPLE_TABLE = "sample_route_data";
+    * public static final String COL_ROUTE_NAME = "sample_route_name";
+    * public static final String COL_STOP_NAME = "sample_stop_name";
+    * public static final String COL_STOP_NUMBER = "sample_route_number";
+    * public static final String COL_X = "sample_x_coordinate";
+    * public static final String COL_Y = "sample_y_coordinate";
+    *
+    * */
+
 
     private static final String DB_LOCATION_TABLE = "location_data";
     private static final String COL_ROUTE_NM = "route_number";
     private static final String COL_CURRENT_X = "X";
     private static final String COL_CURRENT_Y = "Y";
 
-    private static final String DB_ROUTE_CREATE = "create table " + DB_ROUTE_TABLE + " (id integer primary key autoincrement, " + COL_ROUTE_NAME + " text not null, " + COL_STOP_1_NAME + " text not null, " + COL_STOP_1_X + " text not null, " + COL_STOP_1_Y + " text not null, " + COL_STOP_2_NAME + " text not null, " + COL_STOP_2_X + " text not null, " + COL_STOP_2_Y + " text not null, " + COL_STOP_3_NAME + " text not null, " + COL_STOP_3_X + " text not null, " + COL_STOP_3_Y + " text not null);";
+    private static final String DB_ROUTE_CREATE = "create table " + DB_ROUTE_TABLE +
+            " (id integer primary key autoincrement, " +
+            COL_ROUTE_NAME + " text not null, " +
+            COL_STOP_1_NAME + " text not null, " +
+            COL_STOP_1_X + " text not null, " +
+            COL_STOP_1_Y + " text not null, " +
+            COL_STOP_2_NAME + " text not null, " +
+            COL_STOP_2_X + " text not null, " +
+            COL_STOP_2_Y + " text not null, " +
+            COL_STOP_3_NAME + " text not null, " +
+            COL_STOP_3_X + " text not null, " +
+            COL_STOP_3_Y + " text not null);";
+
+    /*
+    *
+    * private static final String DB_SAMPLE_ROUTE_CREATE = "create table " + DB_SAMPLE_ROUTE_DATA +
+    *       " ( " +
+    *       COL_SAMPLE_ROUTE_NAME + " text not null, " +
+    *       COL_SAMPLE_STOP_NAME + " text not null, " +
+    *       COL_SAMPLE_STOP_NUMBER + " text not null, " +
+    *       COL_X + "text not null, " +
+    *       COL_Y + "text not null, " +
+    *       "primary key( " + COL_SAMPLE_ROUTE_NAME + ", " + COL_SAMPLE_STOP_NAME + ");";
+    *
+    * */
 
     private static final String DB_LOCATION_CREATE = "create table " + DB_LOCATION_TABLE +
             " (id integer primary key autoincrement, " +
@@ -61,12 +96,14 @@ public class MyDBAdapter {
         public void onCreate(SQLiteDatabase db) {
             db.execSQL(DB_ROUTE_CREATE);
             db.execSQL(DB_LOCATION_CREATE);
+            //db.execSQL(DB_SAMPLE_ROUTE_CREATE);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             db.execSQL("DROP TABLE IF EXISTS " + DB_ROUTE_TABLE);
             db.execSQL("DROP TABLE IF EXISTS " + DB_LOCATION_TABLE);
+            //db.execSQL("DROP TABLE IF EXISTS " + DB_SAMPLE_ROUTE_TABLE);
             onCreate(db);
         }
     }
@@ -96,6 +133,18 @@ public class MyDBAdapter {
                 null);
     }
 
+    /*
+    public Cursor getSampleSpecificRouteEntry(String rtname)
+    {
+        return database.query(DB_SAMPLE_ROUTE_TABLE,
+        new String[]{COL_ROUTE_NAME, COL_STOP_NAME, COL_STOP_NUMBER, COL_X, COL_Y},
+        COL_ROUTE_NAME + "=?",
+        new String[]{rtname},
+        null,
+        null,
+        null);
+    }
+    */
     public Cursor getLocationEntry(String rtname){
         return database.query(DB_LOCATION_TABLE,
                 new String[]{COL_ROUTE_NM, COL_CURRENT_X, COL_CURRENT_Y},
@@ -128,7 +177,24 @@ public class MyDBAdapter {
         database.insert(DB_ROUTE_TABLE, null, contentValues);
     }
 
-    public void insertCategory(String rtname, String x, String y) {
+    /*
+    *
+    * public void insertSampleEntry(String routeName, String stopName, String stopNumber, String myX, String myY)
+    * {
+    *   ContentValues contentValues = new ContentValues();
+    *
+    *   contentValues.put(COL_ROUTE_NAME, routeName);
+    *   contentValues.put(COL_STOP_NAME, stopName);
+    *   contentValues.put(COL_STOP_NUMBER, stopNumber);
+    *   contentValues.put(COL_X, myX);
+    *   contentValues.put(COL_Y, myY);
+    *
+    *   database.insert(DB_SAMPLE_ROUTE_TABLE, null, contentValues);
+    * }
+    *
+    * */
+
+    public void insertLocation(String rtname, String x, String y) {
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_ROUTE_NM, rtname);
         contentValues.put(COL_CURRENT_X, x);
